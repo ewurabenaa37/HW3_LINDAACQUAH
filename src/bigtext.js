5e39e1a6e67e8a0bf3f039ecefef86394173c890
LIND206 <68594246+LIND206@users.noreply.github.com>
2020-08-11 11:34:38 +0000

    first commit

---
+++ src/bigtext.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './css/layout.css'
+
+function Bigtext() {
+    return(
+        <div>
+            <h1 className= ''>Mavel's Fearless</h1>
+            <p className= 'subtitle'>lorem ipsum dolor sit amet consectetur adipisicing elit. beatae molestias earum eveniet</p>
+            <br/>
+            <div className= 'search-bar'>
+                <input type= 'search' placeholder= 'search'></input>
+
+            </div>
+        </div>
+    )
+}
+
+export default Bigtext;
---
+++ src/wallpaper.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/sample-wallpaper.css'
+
+
+function Wallpaper(props) {
+    return (
+
+        <article class='wallpaper'>
+         <img src={props.pic} alt="Wallpaper #1"></img>
+        
+        </article>
+    )
+}
+export default Wallpaper;
